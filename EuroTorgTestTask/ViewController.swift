d97bd7b85ba9e84fbe691e69408e573330b76612
Ivan Apet <Apet.ivan1@gmail.com>
2020-07-29 19:14:07 +0300

    Initial Commit

---
+++ EuroTorgTestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EuroTorgTestTask
+//
+//  Created by Ivan Apet on 7/29/20.
+//  Copyright © 2020 Ivan Apet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
